ce3ce2bfcf6dd937bb05a3ca9a7d0ce70cd89b79
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 13:58:10 +0100

    update: setAttribute(name, value)

--- setAttribute/main.js
+++ setAttribute/main.js
@@ -1,1 +1,10 @@
 // Your JavaScript code goes here
+
+// setAttribute(name, value)
+
+// document.getElementById('myInput')?.setAttribute('value', 'water bottle');
+
+const myEl = document.createElement('div');
+
+myEl.setAttribute('class', 'dcode1');
+document.body.appendChild(myEl);
