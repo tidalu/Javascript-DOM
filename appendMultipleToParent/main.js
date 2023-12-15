e89e86f7cae01060a53001239ee449312c660f4e
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 18:52:11 +0100

    update: append Multiple children to a parent

--- appendMultipleToParent/main.js
+++ appendMultipleToParent/main.js
@@ -1,1 +1,26 @@
 // Your JavaScript code goes here
+
+function create(text) {
+  let li = document.createElement('li');
+  li.textContent = text;
+  return li;
+}
+
+function append(parent, children) {
+  children.forEach((x) => {
+    parent.appendChild(x);
+  });
+}
+
+const list = document.getElementById('my-list');
+console.log(list);
+
+var items = [
+  create('Dom'),
+  create('Dom1'),
+  create('Dom11'),
+  create('Dom111'),
+  create('Dom1111'),
+];
+
+append(list, items);
