5b6ab49f0dfea659c910462cb54c2ece2b3aaf43
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 14:29:40 +0100

    update: document Fragment

--- ffragments/main.js
+++ ffragments/main.js
@@ -1,1 +1,24 @@
 // Your JavaScript code goes here
+
+/**
+ *
+ * Document fragment :
+ *  <h1>Title</h2>
+ *  <span?>some text  here</span?
+ */
+
+const myList = document.getElementById('myList');
+const docfreg = document.createDocumentFragment();
+
+let item1 = document.createElement('li');
+let item2 = document.createElement('li');
+
+item1.textContent = 'Apples';
+item2.textContent = 'Bananas';
+
+docfreg.appendChild(item1);
+docfreg.appendChild(item2);
+
+myList?.appendChild(docfreg);
+
+console.log(docfreg); /// it is empty now, cuz after appending the fragment we will have nothing left , all the chilrens of the fragment are moved to appended parent
