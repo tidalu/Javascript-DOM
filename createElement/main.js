413cd586e2aedaa706b0381f4b7271f51ed07a89
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 11:48:15 +0100

    update: createElement(tagname, options)

--- createElement/main.js
+++ createElement/main.js
@@ -1,1 +1,25 @@
 // Your JavaScript code goes here
+
+// document.createElement(tagName)
+
+let newElement = document.createElement('input');
+console.dir(newElement); // <input></input>
+
+document.body.appendChild(newElement);
+
+// syntax
+// createElement(tagname, options)
+
+function addElem(length) {
+  const newElem = document.createElement('div');
+  const ulElem = document.createElement('ul');
+  for (let i = 1; i <= length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${i}`;
+    ulElem.appendChild(li);
+  }
+  newElem.appendChild(ulElem);
+  document.body.appendChild(newElem);
+}
+
+addElem(10);
