a4059e03e11af23eefd84f537312be17b5d026cd
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 14:13:23 +0100

    update: textContent - innerHTML

--- textContent/main.js
+++ textContent/main.js
@@ -1,1 +1,12 @@
 // Your JavaScript code goes here
+
+const h2Title = document.getElementById('title');
+
+alert(h2Title?.textContent);
+
+// h2Title.textContent = 'mangoes and bananas';
+
+// innerHTMl -> we can actually put html inside of this , difference is this
+
+h2Title.textContent = '<em> mangoes </em>';
+// h2Title.innerHTML = '<em> mangoes </em>';
