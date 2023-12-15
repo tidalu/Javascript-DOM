330c2d4644298e2b605ccb8fef4c391767e46380
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 15:08:07 +0100

    update: cloneNode(true || )

--- NodeCloneNode/main.js
+++ NodeCloneNode/main.js
@@ -1,1 +1,15 @@
 // Your JavaScript code goes here
+
+// nodeClone allows to aka copy of another node
+const mt = document.getElementById('mylist');
+
+const listChildern = mt?.children;
+console.log(listChildern);
+
+const bananas = listChildern[1];
+console.log(bananas);
+
+const bananasCopy = bananas.cloneNode(true);
+// if we give true as a param , it means clone all the childrens also, otherwise,it does it only for the first layer
+
+mt?.appendChild(bananasCopy);
