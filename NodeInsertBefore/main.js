74bcc347d4bb1db031b731c62c1a984e36e0c5bc
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 15:00:21 +0100

    update: insertBefore(element, beforethis)

--- NodeInsertBefore/main.js
+++ NodeInsertBefore/main.js
@@ -1,1 +1,19 @@
 // Your JavaScript code goes here
+
+const myList = document.getElementById('myList');
+// it takes two Elements , one is an elemnt to insert before it, second el is the actual element to insert ,
+
+const pearsitem = document.getElementById('pearsitem');
+
+console.log(myList);
+console.log(pearsitem);
+
+const oranges = document.createElement('li');
+oranges.textContent = 'oranges';
+
+function addTobeginning(parent, toinsert) {
+  const firstCh = parent.firstChild;
+  parent.insertBefore(toinsert, firstCh);
+}
+
+addTobeginning(myList, oranges);
