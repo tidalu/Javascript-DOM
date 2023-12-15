2644162326103c9331526dde1fb1d2730c1ac6eb
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 16:36:26 +0100

    update: outerHtml

--- outerHTML/main.js
+++ outerHTML/main.js
@@ -1,1 +1,9 @@
 // Your JavaScript code goes here
+
+const title = document.getElementById('title');
+
+//change the outeerhtml
+
+title.outerHTML = '<div id="dom">changed OuterHtml</div>';
+const outerHtml = title?.outerHTML;
+console.log(outerHtml);
