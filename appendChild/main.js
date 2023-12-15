3cda073fe6be76bd34ec5491604cf576aa20274d
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 14:20:02 +0100

    update: appendChild()

--- appendChild/main.js
+++ appendChild/main.js
@@ -1,1 +1,10 @@
 // Your JavaScript code goes here
+
+const myM = document.getElementById('myList');
+
+const list = ['banan', 'orange', 'mango', 'lemon'];
+for (const fr of list) {
+  let li = document.createElement('li');
+  li.textContent = fr;
+  myM?.appendChild(li);
+}
