076df0b81b37a605e89362116e0eb7e31d4e6661
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 15:23:47 +0100

    update: nextElement, nextSiblingElement

--- NodeNextElementSibling/main.js
+++ NodeNextElementSibling/main.js
@@ -1,1 +1,16 @@
 // Your JavaScript code goes here
+
+// this prop is useful for user interfacec
+
+const applesItem = document.querySelector('#myList > li:first-of-type');
+const bananasItem = applesItem?.nextElementSibling;
+console.log(bananasItem);
+
+const nav = document.querySelector('nav');
+const belowNav = nav?.nextElementSibling;
+
+belowNav.style.marginTop = nav?.clientHeight + 'px';
+
+// the difference between nextSibling and nextElementSibling is next element is actually gives the next node and the nextElementSibling
+
+// what did  i mean with node is , next elment is to take anything after the item , like it should not be an html elemen, it can be jsut plain text , so nextElementSibling is to take the next elemtn not the node
