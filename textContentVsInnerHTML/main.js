b2357c9d462f864e9f289a55b696cc047d4fe996
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 18:42:51 +0100

    update: innerHTML vs textContent

--- textContentVsInnerHTML/main.js
+++ textContentVsInnerHTML/main.js
@@ -1,1 +1,5 @@
 // Your JavaScript code goes here
+const myDiv = document.getElementById('myDiv');
+
+myDiv.innerHTML = 'hello my name is <strong>Alif</strong>';
+myDiv.textContent = 'hello my name is <strong>Alif</strong>';
