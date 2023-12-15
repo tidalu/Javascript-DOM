b83f56521a51a42cb66407efc266e8c8b904bf69
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 17:41:17 +0100

    update : querySelectorAll() method

--- querySelector/main.js
+++ querySelector/main.js
@@ -1,1 +1,16 @@
 // Your JavaScript code goes here
+// it returns the first matching element
+const containe2 = document.querySelector('.container2');
+const container2Box = containe2.querySelector('.box');
+const result = document.querySelector('.box');
+// console.log(result);
+
+// querySelectorAll()
+
+const list = document.querySelectorAll('.collect');
+console.log(list); // we could use the return value as an array , butt it si on the console a nodelist
+
+list.forEach((x) => {
+  x.style.color = 'white';
+  x.style.background = 'black';
+});
