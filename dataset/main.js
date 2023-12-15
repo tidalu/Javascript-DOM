9580c8b88dd00e512c2b344f252ff99669c07d25
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 14:50:52 +0100

    update: dataset , removeAttribute

--- dataset/main.js
+++ dataset/main.js
@@ -1,1 +1,10 @@
 // Your JavaScript code goes here
+
+const myDiv = document.getElementById('myDiv');
+
+myDiv.dataset.name = 'bob';
+myDiv.dataset.startedCodingAt = 18;
+myDiv.dataset.name = '';
+
+myDiv.removeAttribute('data-name');
+console.log(myDiv?.dataset);
