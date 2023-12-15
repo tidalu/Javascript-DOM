f8f3a2a0fa6491b21083f69cf50399b9838f38f0
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-15 14:41:33 +0100

    update : classList {add, remove, toggle, replace, contains}

--- classList/main.js
+++ classList/main.js
@@ -1,1 +1,16 @@
 // Your JavaScript code goes here
+const mypara = document.getElementById('myPara');
+mypara?.classList.add('bold', 'big-font');
+
+mypara?.classList.remove('bold');
+
+mypara?.classList.replace('big-font', 'red-bg');
+
+mypara?.classList.toggle('red-bg');
+mypara?.classList.toggle('red-bg');
+
+mypara?.classList.contains('white-text')
+  ? mypara.classList.add('white-text')
+  : mypara?.classList.remove('white-text');
+
+console.log(mypara?.classList);
