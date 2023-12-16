79557332e23045a709b749c0f972417d3dc2845b
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-16 13:54:15 +0100

    update: event delegationi

--- EventDelegation/main.js
+++ EventDelegation/main.js
@@ -1,1 +1,19 @@
 // Your JavaScript code goes here
+const list = document.getElementById('list');
+function colorGen() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+list.addEventListener('click', (a) => {
+  const target = a.target;
+  if (target.matches('li')) {
+    var color = colorGen();
+    var color2 = colorGen();
+    // target.textContent += ' checked';
+    target.style.background = color;
+    target.style.color = color2;
+  }
+});
+
+const newEl = document.createElement('li');
+newEl.textContent = 'alif';
+list?.appendChild(newEl);
