6049a386c4781f5130e56f5db9a59e9430ae39c6
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-16 14:59:05 +0100

    update: isEqualNode()

--- isEqualNode/main.js
+++ isEqualNode/main.js
@@ -1,1 +1,5 @@
 // Your JavaScript code goes here
+const div1 = document.querySelectorAll('div')[0];
+const div2 = document.querySelectorAll('div')[1];
+
+console.log(div1.isEqualNode(div2)); // true
