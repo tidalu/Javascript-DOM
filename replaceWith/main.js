67a20e155b1dfcfb3a6c242caea8ad7ba65cded1
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-16 14:50:29 +0100

    update: replaceWith()

--- replaceWith/main.js
+++ replaceWith/main.js
@@ -1,1 +1,20 @@
 // Your JavaScript code goes here
+
+// replaceWith() method
+//  reaplce a node with another node
+
+const p = document.querySelector('#example p');
+
+const newEl = document.createElement('input');
+newEl.value = 'I amm new';
+
+const btn = document.createElement('button');
+btn.textContent = 'me too';
+btn.addEventListener('click', () => {
+  newEl.value = '';
+});
+// p.replaceWith(newEl, btn, '   and i am somsa');
+
+const strong = document.createElement('strong');
+strong.textContent = ' strong ';
+p.childNodes[0].replaceWith(strong, ' Iam somsa');
