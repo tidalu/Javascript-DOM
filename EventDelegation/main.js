89617992d5c02dc6e84a596afd8329f25c19616b
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-16 14:18:13 +0100

    update: replace child method

--- EventDelegation/main.js
+++ EventDelegation/main.js
@@ -1,19 +1,37 @@
 // Your JavaScript code goes here
 const list = document.getElementById('list');
+
+// Function to generate a random color
 function colorGen() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
-list.addEventListener('click', (a) => {
-  const target = a.target;
-  if (target.matches('li')) {
-    var color = colorGen();
-    var color2 = colorGen();
-    // target.textContent += ' checked';
-    target.style.background = color;
-    target.style.color = color2;
-  }
+
+// Mousemove event listener for the list
+list.addEventListener('mousemove', (event) => {
+  const mouseY = event.clientY;
+  const listTop = list.getBoundingClientRect().top;
+  const listItemHeight = list.querySelector('li').offsetHeight;
+
+  // Calculate the index of the list item to change colors based on mouseY position
+  const index = Math.floor((mouseY - listTop) / listItemHeight);
+
+  // Get the list items
+  const listItems = list.querySelectorAll('li');
+
+  // Change colors for the specific list item
+  listItems.forEach((item, i) => {
+    if (i === index) {
+      var color = colorGen();
+      var color2 = colorGen();
+      item.style.background = color;
+      item.style.color = color2;
+    }
+  });
 });
 
+// Create a new list item with text content 'alif'
 const newEl = document.createElement('li');
 newEl.textContent = 'alif';
+
+// Append the new list item to the list
 list?.appendChild(newEl);
--- replaceChild/main.js
+++ replaceChild/main.js
@@ -1,1 +1,8 @@
 // Your JavaScript code goes here
+const list = document.getElementById('list');
+const css = list.children[1];
+
+const newItem = document.createElement('li');
+newItem.textContent = 'jQuery';
+
+list.replaceChild(newItem, css);
