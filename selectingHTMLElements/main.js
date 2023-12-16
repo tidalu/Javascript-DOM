82b7d499da706571b6528d244ef1c5f0abb6ac2e
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-16 17:07:31 +0100

    update: selecting html elements

--- selectingHTMLElements/main.js
+++ selectingHTMLElements/main.js
@@ -1,1 +1,11 @@
-// Your JavaScript code goes here
+// document.getElementById();
+// document.getElementsByClassName(); live
+// document.getElementsByTagName(); live
+// document.querySelector();
+// document.querySelectorAll();
+
+const h = document.getElementById('heading');
+const s = document.querySelector('#heading');
+console.log(h);
+
+// HTMLCollectio is live in console , but node list is not
