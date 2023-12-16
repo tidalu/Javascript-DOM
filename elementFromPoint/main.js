e45845d14420e7364d1541b4cb2c9fa185d69775
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-16 16:56:14 +0100

    update: elementfromPoint or elementsfromPoint

--- elementFromPoint/main.js
+++ elementFromPoint/main.js
@@ -1,1 +1,9 @@
 // Your JavaScript code goes here
+let pos;
+
+document.addEventListener('mousemove', ({ pageX, pageY }) => {
+  pos = `(${pageX}, ${pageY})`;
+  //   console.log(pos);
+});
+
+// elementFfromPoint or elementsFromPoint
