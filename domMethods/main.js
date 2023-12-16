c5135d2eb65c3e9e8f274344352d2d9ab953d7f6
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-16 17:13:12 +0100

    update: closest()

--- domMethods/main.js
+++ domMethods/main.js
@@ -1,1 +1,9 @@
 // Your JavaScript code goes here
+
+//  closest()
+
+document.querySelectorAll('button')?.forEach((element) => {
+  element.addEventListener('click', () => {
+    console.log(element.closest('tr')); // we can pas class also id maybe
+  });
+});
